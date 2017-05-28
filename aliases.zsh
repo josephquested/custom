63f060e088070b0ccab723dc702e5c10b8ea5f73
josephquested <josephquested@gmail.com>
2017-05-28 13:34:57 +1200

    initial commit

---
+++ aliases.zsh
@@ -0,0 +1,41 @@
+alias git=hub
+
+alias ali="atom ~/.oh-my-zsh/custom/aliases.zsh"
+alias zshrc="atom ~/.oh-my-zsh"
+
+alias sr="~/Documents/steamroom"
+
+alias doc="~/Documents"
+alias dl="~/Downloads"
+alias eda="~/Documents/eda"
+alias notes="~/Documents/notes"
+
+alias cur="~/Documents/ISLAND"
+alias curs="~/Documents/ISLAND/Assets/Scripts"
+
+alias gcnotes='gaa;gcmsg "notes";gp'
+
+alias u="open /Applications/Unity/Unity.app"
+alias rrm="rm -rf"
+alias gs="git status"
+alias gpum="git push --set-upstream origin master"
+
+alias c="open /Applications/Google\ Chrome.app"
+alias cgit="open http://www.github.com/josephquested"
+alias cmail="open https://inbox.google.com"
+alias ccal="open https://calendar.google.com"
+
+alias a="open /Applications/Atom.app"
+alias ao="atom"
+alias aoa="atom ."
+
+alias oin="open index.html"
+alias l3="open http://localhost:3000/"
+
+alias o="open"
+alias oa="open ."
+
+alias ns="npm start"
+alias nw="npm run watch"
+alias nrs="npm run server"
+alias nt="npm test"
